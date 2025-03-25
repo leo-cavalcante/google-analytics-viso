721aa2cc137ab6e7264d16c899cf54526d61d077
Leonardo Cavalcante Araújo <68561078+leo-cavalcante@users.noreply.github.com>
2025-03-25 11:05:11 +0100

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -9,20 +9,17 @@ from datetime import date
 import plotly_express as px
 import matplotlib.pyplot as plt
 from bokeh.plotting import figure
-from google.analytics.data_v1beta.types import Metric
-from google.analytics.data_v1beta.types import OrderBy
-from google.analytics.data_v1beta.types import DateRange
-from google.analytics.data_v1beta.types import Dimension
-from google.analytics.data_v1beta.types import RunReportRequest
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 
 # GLOBAL VARIABLES
 property_id = "386101877"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google-analytics-viso-service-account.json'
 client = BetaAnalyticsDataClient()
-
+# output_df = st.session_state.output_df
+# comp_df = st.session_state.comp_df
+    
 ## STREAMLIT APP - MAIN STRUCTURE
-st.set_page_config(layout="wide", initial_sidebar_state='collapsed', page_icon='viso-logo.jpg', page_title='VISO MKT Digital')   # Use the full page instead of a narrow central column
+st.set_page_config(layout="wide", initial_sidebar_state='auto', page_icon='viso-logo.jpg', page_title='VISO MKT Digital')   # Use the full page instead of a narrow central column
 
 intro1, intro2 = st.columns([0.25, 0.75])
 with intro1:
@@ -35,60 +32,41 @@ st.sidebar.header("Filtres")
 
 one_year = date.today() - dateutil.relativedelta.relativedelta(months=11)
 
-# scol1, scol2 = st.sidebar.columns(2)
-# with scol1:
 start_date_input = st.sidebar.date_input("Date de Début d'Analyse", value=date(one_year.year, one_year.month, 1),
                                             min_value="2016-01-01", max_value=date(date.today().year, date.today().month, date.today().day-1))
 end_date_input = st.sidebar.date_input("Date de Fin d'Analyse", value="today", min_value=start_date_input,
                                             max_value=date(date.today().year, date.today().month, date.today().day-1))
 no_days = end_date_input - start_date_input
-# with scol2:
-#     start_date_comparison = st.sidebar.date_input("Début Comparaison", value=date(start_date_input.year-1, start_date_input.month, start_date_input.day),
-#                                                 min_value="2016-01-01", max_value=start_date_input - dateutil.relativedelta.relativedelta(days=1))
-#     end_date_comparison = st.sidebar.date_input("Fin Comparaison", value=min(date(end_date_input.year-1, end_date_input.month, end_date_input.day),start_date_input),
-#                                                 min_value="2016-01-01", max_value=start_date_input - dateutil.relativedelta.relativedelta(days=1))
 
 start_date_comparison = date(start_date_input.year-1, start_date_input.month, start_date_input.day)
 end_date_comparison = date(end_date_input.year-1, end_date_input.month, end_date_input.day)
 no_days_comparison = end_date_comparison - start_date_comparison
-st.sidebar.markdown("Dates de Comparaison: du {} au {}".format(start_date_comparison.strftime("%d %B %Y"),end_date_comparison.strftime("%d %B %Y")))
+st.sidebar.caption("Période de Comparaison:\ndu {}\nau {}".format(start_date_comparison.strftime("%d %B %Y"),end_date_comparison.strftime("%d %B %Y")))
 st.sidebar.divider()
 
 ## DATA COLLECTION
 @st.cache_data
-def request_ga_data(property_id, start_date_input=start_date_input, end_date_input=end_date_input):
-    return RunReportRequest(
-            property='properties/'+property_id,
-            dimensions=[Dimension(name="yearMonth"), # Dimension(name="year"), Dimension(name="month"), # Dimension(name="week"), # Dimension(name="date"),
-                        Dimension(name="country"), # Dimension(name="sessionSourceMedium")], # Dimension(name="sessionMedium"), # Dimension(name="defaultChannelGroup")],       # NOT POSSIBLE TO CALCULATE NEW VISITORS
-                        Dimension(name="firstUserDefaultChannelGroup")],
-            metrics=[Metric(name="activeUsers"),
-                     Metric(name="newUsers"),
-                     Metric(name="Sessions"),
-                     Metric(name="engagedSessions"), #  Metric(name="bounceRate"),
-                     Metric(name="screenPageViews"),                    ## >> screen_view + page_view events
-                     Metric(name="averageSessionDuration")],
-            order_bys = [OrderBy(dimension = {'dimension_name': 'yearMonth'}), # OrderBy(dimension = {'dimension_name': 'year'}), OrderBy(dimension = {'dimension_name': 'month'}),
-                        OrderBy(dimension = {'dimension_name': 'country'}),
-                        OrderBy(dimension = {'dimension_name': 'firstUserDefaultChannelGroup'})],
-            date_ranges=[DateRange(start_date=start_date_input.strftime("%Y-%m-%d"), end_date=end_date_input.strftime("%Y-%m-%d"))],
-        )
+def request_df_and_comp(_client, property_id, start_date_input=start_date_input, end_date_input=end_date_input, start_date_comparison=start_date_comparison, end_date_comparison=end_date_comparison):
+    request = request_ga_data(property_id, start_date_input, end_date_input)
+    output_df = format_report(client, request)
+    
+    request_comp = request_ga_data(property_id, start_date_comparison, end_date_comparison)
+    comp_df = format_report(client, request_comp)
+    
+    output_df[['activeUsers','newUsers','Sessions','engagedSessions','screenPageViews','averageSessionDuration']] = output_df[['activeUsers','newUsers','Sessions','engagedSessions','screenPageViews','averageSessionDuration']].fillna(0)
+    comp_df[['activeUsers','newUsers','Sessions','engagedSessions','screenPageViews','averageSessionDuration']] = comp_df[['activeUsers','newUsers','Sessions','engagedSessions','screenPageViews','averageSessionDuration']].fillna(0)
+    comp_df['yearMonth'] = comp_df['yearMonth'].map(lambda x: str(int(x)+100))
+    
+    return output_df, comp_df
     
 ## TABLE OUTPUT
-request = request_ga_data(property_id, start_date_input, end_date_input)
-output_df = format_report(client, request)
-output_df[['activeUsers','newUsers','Sessions','engagedSessions','screenPageViews','averageSessionDuration']] = output_df[['activeUsers','newUsers','Sessions','engagedSessions','screenPageViews','averageSessionDuration']].fillna(0)
-# output_df.reset_index(inplace=True)
-
-request_comp = request_ga_data(property_id, start_date_comparison, end_date_comparison)
-comp_df = format_report(client, request_comp)
-comp_df['yearMonth'] = comp_df['yearMonth'].map(lambda x: str(int(x)+100))
+output_df, comp_df = request_df_and_comp(client, property_id, start_date_input, end_date_input, start_date_comparison, end_date_comparison)
 
 # FILTERS SIDEBAR -- PART 2
 country_filter = st.sidebar.multiselect("Pays:", options=output_df['country'].unique())
 firstUserDefaultChannelGroup_filter = st.sidebar.multiselect("Canal d'Acquisition:", options=output_df['firstUserDefaultChannelGroup'].unique())
 st.sidebar.divider()
-top_results = st.sidebar.slider("Nombre de TOP résultats à afficher:", min_value=5, max_value=50, value=5, step=5)
+top_results = st.sidebar.slider("Nombre de TOP résultats à afficher:", min_value=5, max_value=50, value=10, step=5)
 st.sidebar.divider()
 st.sidebar.button("Download Excel Output", on_click=export_to_excel(output_df))
 # st.sidebar.download_button("Download Excel Output", data=output_df, file_name=date.today().strftime("%Y-%m-%d at %H-%m-%s") + '.xlsx', on_click=export_to_excel(output_df))
@@ -97,15 +75,13 @@ st.sidebar.button("Download Excel Output", on_click=export_to_excel(output_df))
 output_df = df_preparation(output_df=output_df, country_filter=country_filter, firstUserDefaultChannelGroup_filter=firstUserDefaultChannelGroup_filter)
 comp_df = df_preparation(output_df=comp_df, country_filter=country_filter, firstUserDefaultChannelGroup_filter=firstUserDefaultChannelGroup_filter)
 
-# st.divider()
-
 tab1, tab2 = st.tabs(["Graphs", "Tables"])
 with tab1:
     ## GRAPH 1 -- FUNNEL MARKETING
-    # Aggregating absolute values on dimensions of interest
     st.subheader(f'\nFunnel Marketing Digital')
     funnel_chart = build_funnel(output_df)
     fig = px.funnel(funnel_chart, x='Nombre', y='Étape')
+    fig.update_layout(font=dict(size=12))
     st.plotly_chart(fig)
 
     st.divider()
@@ -113,30 +89,26 @@ with tab1:
     ## GRAPH 2
     st.subheader(f'\nUtilisateurs : Actifs, Nouveaux & Bounces')
     year_month = build_year_month(output_df=output_df, comp_df=comp_df)
-    # st.write(max(output_df[output_df['country']=='Andorra']['Sessions']))
-    # st.write(year_month['Sessions'])
-    # st.write(year_month)
     
     base = alt.Chart(year_month).encode(
         x=alt.X('yearMonth:N', axis=alt.Axis(labelAngle=-0, title='', format="time", formatType="YYYY MMM"), sort='descending'), #labelAngle=-45,
         y=alt.Y('Sessions:Q', axis=alt.Axis(title='')))
 
-    bar_active = base.mark_bar(color='blue', xOffset=-11, width=20).encode(y='activeUsers:Q')
-    text_active = bar_active.mark_text(align='center', xOffset=-11, baseline='top', dx=0, color='white', size=10).encode(text='activeUsers:Q',
+    bar_active = base.mark_bar(color='blue', xOffset=-14, width=27).encode(y='activeUsers:Q')
+    text_active = bar_active.mark_text(align='center', xOffset=-14, baseline='top', dx=0, color='white', size=12).encode(text='activeUsers:Q',
                 x=alt.X('yearMonth:N', axis=alt.Axis(labelAngle=-0, title='', format="time", formatType="YYYY MMM"), sort='descending'), #labelAngle=-45,
                 y=alt.Y('activeUsers:Q', stack='zero'),)
 
-    bar_new = base.mark_bar(color='green', opacity=1, xOffset=11, width=20).encode(y='newUsers:Q')
-    text_new = bar_new.mark_text(align='center', xOffset=11, baseline='top', dx=0, color='white', size=10).encode(text='newUsers')
+    bar_new = base.mark_bar(color='green', opacity=1, xOffset=14, width=27).encode(y='newUsers:Q')
+    text_new = bar_new.mark_text(align='center', xOffset=14, baseline='top', dx=0, color='white', size=12).encode(text='newUsers')
     
-    bar_sessions = base.mark_bar(color='darkblue', opacity=1, xOffset=-11, width=20).encode(y=alt.Y('Sessions:Q', axis=alt.Axis(title='')))
-    text_sessions = bar_sessions.mark_text(align='center', xOffset=-11, baseline='top', dx=0, color='white', size=10).encode(text='Sessions')
+    bar_sessions = base.mark_bar(color='darkblue', opacity=1, xOffset=-14, width=27).encode(y=alt.Y('Sessions:Q', axis=alt.Axis(title='')))
+    text_sessions = bar_sessions.mark_text(align='center', xOffset=-14, baseline='top', dx=0, color='white', size=12).encode(text='Sessions')
 
-    alt.Color(value='lightgray')
     line_bounce = base.mark_line(color='#f93d48',dx=2).encode(y=alt.Y('bounces'), text='bounceRate_txt:N')  #.encode(y='sum(bounces)')
-    text_bounce = line_bounce.mark_text(align='center', xOffset=0, baseline='bottom', dx=15, color='#fa8072', opacity=1, size=13).encode(text='bounceRate_txt')
+    text_bounce = line_bounce.mark_text(align='center', xOffset=0, baseline='bottom', dx=15, color='#fa8072', opacity=1, size=12).encode(text='bounceRate_txt')
 
-    bar_users = st.altair_chart(bar_sessions + text_sessions + bar_new + text_new + bar_active + text_active + line_bounce + text_bounce,
+    bar_users = st.altair_chart(bar_sessions + text_sessions + bar_active + text_active + bar_new + text_new + line_bounce + text_bounce,
                                 theme="streamlit", use_container_width=True) # on_select="rerun")
     st.divider()
     
@@ -158,8 +130,7 @@ with tab1:
 
 
     st.markdown(f'\nSessions Engagées')
-
-    base_d = alt.Chart(channel).mark_bar().encode(x=alt.X('engagedSessions:Q', sort='ascending', stack='normalize'),
+    base_d = alt.Chart(channel).mark_bar().encode(x=alt.X('engagedSessions:Q', title='', sort='ascending', stack='normalize'),
                                                             y=alt.Y('yearMonth:N', title='', sort='descending'),
                                                             tooltip=['engagedSessions_Percent:N','engagedSessions:Q','Channel:N','yearMonth:N']) #, type='temporal'
     chart_d = base_d.mark_bar().encode(color="Channel")
@@ -170,12 +141,11 @@ with tab1:
     d = st.altair_chart(chart_d + d_text, use_container_width=True)
     
 ## GRAPH 5
-    
 with tab2:
     st.subheader(f'\nPrincipaux KPIs de Performance')
     # st.dataframe(data=year_month.style.highlight_max(axis=0,subset=['bounceRate'],color='red',).highlight_max(axis=0,subset=['engagedSessionsRate','newUsersRate','returningUsersRate'],color='#34a853'),
-    st.dataframe(data=year_month, #.style.applymap(color_background, subset=['bounceRate','engagedSessionsRate','newUsersRate','returningUsersRate'])
-                    height=None, hide_index=True,
+    st.dataframe(data=year_month.style.applymap(color_rate, subset=['bounceRate','engagedSessionsRate','newUsersRate','returningUsersRate']),
+                    height=None, hide_index=True, on_select="rerun",
                     column_order=['yearMonth','Sessions','engagedSessionsRate','bounceRate','activeUsers','returningUsersRate','newUsersRate','avgScreenViews','avgSessionDuration'],
                 column_config={
         "yearMonth": st.column_config.DateColumn("Année - Mois", format="YYYY - MMM", pinned=True),
@@ -196,16 +166,16 @@ with tab2:
     
     cmap = plt.cm.get_cmap('PRGn')
     st.dataframe(data=year_month.style.background_gradient(cmap=cmap,vmin=-1,vmax=1,axis=None).applymap(color_background, subset=['Sessions_vs_LY','engagedSessions_vs_LY','bounces_vs_LY','activeUsers_vs_LY','returningUsers_vs_LY','newUsers_vs_LY']), #.set_properties(**{'text-align': 'center'}).set_table_styles([{'selector': 'th', 'props': [('text-align', 'center')]}])
-                    height=None, hide_index=True,
+                    height=None, hide_index=True, on_select="rerun",
                     column_order=['yearMonth','Sessions_vs_LY','engagedSessions_vs_LY','bounces_vs_LY','activeUsers_vs_LY','returningUsers_vs_LY','newUsers_vs_LY'],
                 column_config={
         "yearMonth": st.column_config.DateColumn("Année - Mois", format="YYYY - MMM", pinned=True),
-        "Sessions_vs_LY": st.column_config.NumberColumn("Sessions vs LY", format="percent",min_value=0,max_value=1,width="small"),
-        "engagedSessions_vs_LY": st.column_config.NumberColumn("Engagées vs LY",format="percent",min_value=0,max_value=1,width="small"),
-        "bounces_vs_LY": st.column_config.NumberColumn("Bounces vs LY",help="% of users exiting website as soon as landing",format="percent",min_value=0,max_value=1,width="small"),
-        "activeUsers_vs_LY": st.column_config.NumberColumn("Utilisateurs vs LY",format="percent",min_value=0,max_value=1,width="small"),
-        "returningUsers_vs_LY": st.column_config.NumberColumn("Retour vs LY",format="percent",min_value=0,max_value=1,width="small"),
-        "newUsers_vs_LY": st.column_config.NumberColumn("Nouveaux vs LY",format="percent",min_value=0,max_value=1,width="small"),
+        "Sessions_vs_LY": st.column_config.NumberColumn("Sessions vs LY", format="percent",min_value=0,max_value=1),
+        "engagedSessions_vs_LY": st.column_config.NumberColumn("Engagées vs LY",format="percent",min_value=0,max_value=1),
+        "bounces_vs_LY": st.column_config.NumberColumn("Bounces vs LY",help="% of users exiting website as soon as landing",format="percent",min_value=0,max_value=1),
+        "activeUsers_vs_LY": st.column_config.NumberColumn("Utilisateurs vs LY",format="percent",min_value=0,max_value=1),
+        "returningUsers_vs_LY": st.column_config.NumberColumn("Retour vs LY",format="percent",min_value=0,max_value=1),
+        "newUsers_vs_LY": st.column_config.NumberColumn("Nouveaux vs LY",format="percent",min_value=0,max_value=1),
     })
 
     st.divider()
